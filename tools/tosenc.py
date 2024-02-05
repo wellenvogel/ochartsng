29a26857a348cc48dc3e74b0822f5981e0449de9
andreas <andreas@wellenvogel.de>
2024-02-05 17:30:09 +0100

    add zip input handling to tools

--- tools/tosenc.py
+++ tools/tosenc.py
@@ -25,6 +25,7 @@
  ***************************************************************************
  *
 '''
+import copy
 import glob
 import re
 import struct
@@ -51,11 +52,103 @@ def convertGdal(ifile,ofile):
         raise Exception("converting %s to %s returned %d"%(ifile,ofile,proc.returncode))
 
 def usage():
-    print("usage: %s [-b basedir] [-d s57datadir] [-s scale] [-e none|ext|all] [-i] [-c setname] infileOrDir outfileOrDir",sys.argv[0])
+    print("usage: %s [-b basedir] [-d s57datadir] [-s scale] [-e none|ext|all] [-i] [-c setname] infileOrDir outfileOrDir"%sys.argv[0])
+
+class Context:
+    TMP_SUB="__tmp"
+    IN_SUB="__in"
+    SQL_EXT="sqlite"
+    OUT_EXT="senc"
+    CNV_EXT=['000','geojson']
+    def __init__(self,tmpDir:tempfile.TemporaryDirectory=None,
+                 zipFile: zipfile.ZipFile=None,
+                 options:sqlite2senc.Options=None,
+                 setname:str=None,
+                 ignoreErrors:bool=False
+                 ):
+        self.zipFile=zipFile
+        if zipFile is not None:
+            self.zipName=zipFile.filename
+        else:
+            self.zipName=None
+        self.tmpDir=tmpDir
+        self.options=options if options is not None else sqlite2senc.Options()
+        self.setname=setname
+        self.ignoreErrors=ignoreErrors
+        self.errors=[]
+        self.numFiles=0
+    def openZip(self,filename):
+        self.zipName=filename
+        tmpname=filename+".tmp"
+        self.zipFile=zipfile.ZipFile(tmpname,"w",compression=zipfile.ZIP_DEFLATED)
+    def getTmpDir(self,subDir=None):
+        if self.tmpDir is None:
+            self.tmpDir=tempfile.TemporaryDirectory()
+        rt=self.tmpDir.name
+        if subDir is not None:
+            rt=os.path.join(rt,subDir)
+            if not os.path.exists(rt):
+                os.makedirs(rt)
+                if not os.path.isdir(rt):
+                    raise Exception("unable to create %s"%rt)
+        return rt
+    def addToZip(self,fileName):
+        if self.zipFile is None:
+            return
+        arcname=os.path.basename(fileName)
+        if self.setname is None:
+            raise Exception("need a setname for creating zipfiles")
+        arcname=self.setname+"/"+arcname
+        self.zipFile.write(fileName,arcname)
+    def finalize(self):
+        if self.zipFile is not None:
+            self.zipFile.writestr(self.setname+"/Chartinfo.txt","ChartInfo:%s\n"%self.setname)
+            tmpname=self.zipFile.filename
+            self.zipFile.close()
+            os.replace(tmpname,self.zipName)
+    def addError(self,filename,reason=None):
+        self.errors.append({'name':filename,'reason':reason})
+    def printErrors(self):
+        if len(self.errors) < 1:
+            return
+        print("Errors:")
+        for e in self.errors:
+            print("  %s: %s",e['name'],e['reason'] or '')
+    def addHandledFiles(self,filename):
+        self.numFiles+=1
+
+def handleSingleFile(context:Context,ifile:str,ofile:str):
+    name,ext=os.path.splitext(os.path.basename(ifile))
+    ext=ext[1:]
+    options=copy.deepcopy(context.options)
+    if options.basedir is None:
+        options.basedir=os.path.dirname(iname)
+    sqliteFile=None
+    if ext in Context.CNV_EXT:
+        tmpName=os.path.join(context.getTmpDir(Context.TMP_SUB),name+"."+Context.SQL_EXT)
+        try:
+            convertGdal(ifile,tmpName)
+            if not os.path.exists(tmpName):
+                raise Exception("%s not created from %s"%(tmpName,ifile))
+            sqliteFile=tmpName
+        except Exception as e:
+            if not context.ignoreErrors:
+                raise
+            context.addError(iname,str(e))
+            return
+    elif ext == Context.SQL_EXT:
+        sqliteFile=ifile
+    else:
+        sqlite2senc.warn("unknown file type %s",iname)
+        context.addError(iname,"unknown file type")
+        return
+    sqlite2senc.main(sqliteFile,ofile,options)
+    context.addToZip(ofile)
+    context.addHandledFiles(ifile)
+
+
 M_FILES=0
 M_DIR=1
-M_ZIPDIR=2
-M_ZIPFILE=3
 if __name__ == '__main__':
     s57dir=os.path.join(os.path.dirname(__file__),"..","provider","s57static")
     basedir=None
@@ -96,103 +189,58 @@ if __name__ == '__main__':
     if not os.path.exists(iname):
         err("%s not found",iname)
     oname=args[1]
+    origOname=oname
     mode=M_FILES
-    errors=[]
+    context=Context(options=options,setname=setname)
     if os.path.isdir(iname):
         mode=M_DIR
+    ifname,iext=os.path.splitext(iname)
+    if iext.upper()=='.ZIP':
+        mode=M_DIR
+        inzip=zipfile.ZipFile(iname,"r")
+        unzipDir=context.getTmpDir(Context.IN_SUB)
+        log("extracting %s to %s",iname,unzipDir)
+        inzip.extractall(unzipDir)
+        iname=unzipDir
     name,ext=os.path.splitext(oname)
     if ext.upper()==".ZIP":
-        mode=M_ZIPDIR if mode == M_DIR else M_ZIPFILE
-    if os.path.exists(oname):
-        if mode == M_DIR and not os.path.isdir(oname):
-            err("found existing outfile %s but expected a directory",oname)
-        if mode == M_FILES and os.path.isdir(oname):
-            base,dummy=os.path.splitext(os.path.basename(iname))
-            oname=os.path.join(oname,base+".senc")
-            log("creating outfile %s",oname)
-        if mode == M_ZIPFILE or mode == M_ZIPDIR:
-            if os.path.isdir(oname):
-                err("zip outfile %s exists as an directory",oname)
-    if mode == M_DIR and not os.path.exists(oname):
-        log("creating directory %s",oname)
-        os.makedirs(oname)
-        if not os.path.isdir(oname):
-            err("unable to create directory %s",oname)
-    tmpDir=None
-    zipOut=None
-    if mode == M_ZIPFILE or mode == M_ZIPDIR:
-        tmpDir=tempfile.TemporaryDirectory()
-        zipOut=zipfile.ZipFile(oname,mode='w',compression=zipfile.ZIP_DEFLATED)
+        if os.path.isdir(oname):
+            err("zip outfile %s exists as an directory",oname)
         if setname is None:
             setname,ext=os.path.splitext(os.path.basename(oname))
-    if mode == M_FILES or mode == M_ZIPFILE:
-        name,ext=os.path.splitext(os.path.basename(iname))
-        ext=ext[1:]
-        if not baseDirArg:
-            options.basedir=os.path.dirname(iname)
-        sqliteFile=None
-        if ext == '000' or ext == 'geojson':
-            tmpDir=tempfile.TemporaryDirectory()
-            tmpName=os.path.join(tmpDir.name,name+".sqlite")
-            try:
-                convertGdal(iname,tmpName)
-                if not os.path.exists(tmpName):
-                    raise Exception("%s not created from %s"%(tmpName,iname))
-                sqliteFile=tmpName
-            except Exception as e:
-                if not ignoreErrors:
-                    raise
-                errors.append(iname)
-        if ext == 'sqlite':
-            sqliteFile=iname
-        else:
-            err("unknown file type %s",iname)
-        if mode==M_ZIPFILE:
-            outfile=os.path.join(tmpDir.name,name+".senc")
-        else:
-            outfile=oname
-        sqlite2senc.main(sqliteFile,outfile,options)
-        if mode == M_ZIPFILE:
-            zipOut.write(outfile,setname+"/"+name+".senc")
+            context.setname=setname
+        context.openZip(oname)
+        oname=context.getTmpDir(Context.TMP_SUB)
     else:
-        KNOWN_EXT=['sqlite','000','geojson']
+        if os.path.exists(oname):
+            if mode == M_DIR and not os.path.isdir(oname):
+                err("found existing outfile %s but expected a directory",oname)
+            if mode == M_FILES and os.path.isdir(oname):
+                base,dummy=os.path.splitext(os.path.basename(iname))
+                oname=os.path.join(oname,base+".senc")
+                log("creating outfile %s",oname)
+        if mode == M_DIR and not os.path.exists(oname):
+            log("creating directory %s",oname)
+            os.makedirs(oname)
+            if not os.path.isdir(oname):
+                err("unable to create directory %s",oname)
+    if mode == M_FILES:
+        handleSingleFile(context,iname,oname)
+    else:
+        KNOWN_EXT=Context.CNV_EXT+[Context.SQL_EXT]
         for file in glob.glob(iname+"/**",recursive=True):
             if os.path.isdir(file):
                 continue
             name,ext=os.path.splitext(file)
             ext=ext[1:]
             basename,dummy=os.path.splitext(os.path.basename(file))
-            ofile=os.path.join(oname,basename+".senc")
+            ofile=os.path.join(oname,basename+"."+Context.OUT_EXT)
             if not baseDirArg:
                 options.basedir=os.path.dirname(file)
             if ext in KNOWN_EXT:
                 log("handling file %s",file)
-                ifile=None
-                if ext != 'sqlite':
-                    if tmpDir is None:
-                        tmpDir=tempfile.TemporaryDirectory()
-                    tmpFile=os.path.join(tmpDir.name,basename+".sqlite")
-                    try:
-                        convertGdal(file,tmpFile)
-                        if not os.path.exists(tmpFile):
-                            raise Exception("%s does not exist after conversion from %s"%(tmpFile,file))
-                        ifile=tmpFile
-                    except Exception as e:
-                        if not ignoreErrors:
-                            raise
-                        else:
-                            errors.append(file)
-                else:
-                    ifile=file
-                if ifile is not None:
-                    if mode == M_ZIPDIR:
-                        ofile=os.path.join(tmpDir.name,basename+".senc")
-                    sqlite2senc.main(ifile,ofile,options)
-                    if mode == M_ZIPDIR:
-                        zipOut.write(ofile,setname+"/"+basename+".senc")
-    if len(errors) > 0:
-        print("ERRORS in files:","\n".join(errors))
-    if zipOut is not None:
-        zipOut.writestr(setname+"/Chartinfo.txt","ChartInfo:%s\n"%setname)
-        zipOut.close()
-    log("done")
+                handleSingleFile(context,file,ofile)
+    context.finalize()
+    context.printErrors()
+    log("created %s with %d files",origOname,context.numFiles)
+    sys.exit(1 if len(context.errors)>0 else 0)
