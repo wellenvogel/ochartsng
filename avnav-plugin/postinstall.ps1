4ba29832d1db02ebd2b97473ddc27349a2745b19
andreas <andreas@wellenvogel.de>
2024-05-16 07:31:09 +0200

    add download for msvc dlls for windows

--- avnav-plugin/postinstall.ps1
+++ avnav-plugin/postinstall.ps1
@@ -9,4 +9,17 @@ else {
 $pythonDir = "python"
 Write-Host "pip install $name"
 $res = (Start-Process -WorkingDirectory $targetBase -FilePath "$targetBase\$pythonDir\python.exe" -ArgumentList "-m", "pip","install", $name -PassThru -Wait -NoNewWindow)
+$download = New-Object System.Net.WebClient
+$destination="$targetBase\$pythonDir\Lib\site-packages"
+$dlls=@("msvcp140.dll","vcruntime140_1.dll")
+$dlbase="https://www.wellenvogel.net/software/avnav/downloads/supplement"
+foreach ($dll in $dlls) {
+    $downloadName="$destination\$dll"
+    $url="$dlbase/$dll"
+    Write-Host "downloading $downloadName from $url"
+    $download.DownloadFile($url,$downloadName)
+    if (-Not ($null=Test-Path "$downloadName")){
+        Write-Host "WARNING: download for $downloadName failed"
+    }
+}
 exit $res
